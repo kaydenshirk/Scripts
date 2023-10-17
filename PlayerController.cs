1b645b690d866c7c0464fa1bea7f7bad34dc18aa
kaydenshirk <811006@sylvaniastudents.org>
2023-10-17 10:02:54 -0400

    finished scripts

---
+++ PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 60.0f;
+    private float horizontalInput;
+    private float forwardInput; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player input
+        horizontalInput = Input.GetAxis ("Horizontal");
+        forwardInput = Input.GetAxis ("Vertical");
+        //truck movement
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
